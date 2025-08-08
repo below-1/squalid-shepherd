a546601e3a9ccab8502a86980744bf1b39b41012
Jordan Meta <jordan@esphere.id>
2025-08-08 23:07:56 +0800

    add icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import icon from 'astro-icon';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://example.com',
+  integrations: [mdx(), icon()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
